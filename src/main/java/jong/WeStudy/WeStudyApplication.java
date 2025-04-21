46558829cd774d7375013a5945303c733e60a25c
jonghyun0429p <bwhdgus0429@naver.com>
2025-04-21 11:58:07 +0900

    Init : 스프링 환경 구축

---
+++ src/main/java/jong/WeStudy/WeStudyApplication.java
@@ -0,0 +1,13 @@
+package jong.WeStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jong/WeStudy/WeStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package jong.WeStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
