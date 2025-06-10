bb45f0e8c378f4d06dff5a61eeef3d718f54b9b4
Jonghyeon0429p <bwhdgus0429@naver.com>
2025-06-10 16:30:44 +0900

    chore: 기본 파일 수정
    mysql driver을 사용하다가 mariadb driver 존재로 수정
    도커 환경에서 profiles를 조절 못하는 문제가 발생해 yml파일로 직접 지정
    기본 admin 유저 기입 init 설정

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,17 @@
+-- 1️. 데이터베이스 생성 (이미 있을 경우 skip 가능)
+CREATE DATABASE IF NOT EXISTS westudy DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+-- 2. 해당 데이터베이스 사용
+USE westudy;
+
+-- 3️. user 테이블 생성
+CREATE TABLE IF NOT EXISTS user (
+                                    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                    username VARCHAR(50) NOT NULL UNIQUE,
+                                    password VARCHAR(255) NOT NULL,
+                                    email VARCHAR(100) NOT NULL UNIQUE,
+                                    nickname VARCHAR(50) NOT NULL,
+                                    phone_number VARCHAR(20) NOT NULL,
+                                    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                                    role VARCHAR(20) DEFAULT 'USER_USER'
+);
