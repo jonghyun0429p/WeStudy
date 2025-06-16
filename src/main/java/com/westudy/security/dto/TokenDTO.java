78498dc674b2514dfac48c4bbd194f752a8f4976
Jonghyeon0429p <bwhdgus0429@naver.com>
2025-06-17 00:29:05 +0900

    feat: token 사용하기 위한 DTO, mapper작성
    token을 활용하기 위해서 DTO와 mapper interface, xml파일을 작성해 활용

---
+++ src/main/java/com/westudy/security/dto/TokenDTO.java
@@ -0,0 +1,11 @@
+package com.westudy.security.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TokenDTO {
+    private long id;
+    private String token;
+}
---
+++ src/main/java/com/westudy/security/mapper/TokenMapper.java
@@ -0,0 +1,13 @@
+package com.westudy.security.mapper;
+
+import com.westudy.security.dto.TokenDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TokenMapper {
+    void insertToken(long userid, String token);
+
+    void deleteByUserid(long userid);
+
+    TokenDTO findByuserid(long userid);
+}
