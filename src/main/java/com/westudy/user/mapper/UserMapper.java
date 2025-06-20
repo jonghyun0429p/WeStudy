90280cd1b55db94c4e20d5b6b814f942eb70be9f
Jonghyeon0429p <bwhdgus0429@naver.com>
2025-06-20 16:33:51 +0900

    feat: UserMapper 기능 추가
    - 리프레시 토큰을 통해 엑세스 토큰을 재발급하기 위해서 userId로 User 객체를 찾아서 가져와야하므로 기능추가

--- src/main/java/com/westudy/user/mapper/UserMapper.java
+++ src/main/java/com/westudy/user/mapper/UserMapper.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
+    User findByUserId(Long userId);
     User findByUsername(String username);
     User findByEmail(String email);
     void insertUser(User user);
