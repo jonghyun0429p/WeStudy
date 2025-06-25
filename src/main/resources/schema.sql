528e4f9e0f8509f1fffbaa930b35a23fd862ecbb
Jonghyeon0429p <bwhdgus0429@naver.com>
2025-06-25 18:56:31 +0900

    fix: schema 기본값 추가
    - schema.sql을 추가된 엔티티에 맞게 테이블을 생성하도록함

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -13,11 +13,63 @@ CREATE TABLE IF NOT EXISTS user (
                                     nickname VARCHAR(50) NOT NULL,
                                     phone_number VARCHAR(20) NOT NULL,
                                     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                                    delete_at DATETIME,
                                     role VARCHAR(20) DEFAULT 'USER_USER'
 );
 
 -- 4. token 테이블 생성(refresh)
 CREATE TABLE IF NOT EXISTS token (
-                                    userid BIGINT KEY,
+                                    user_id BIGINT KEY NOT NULL UNIQUE,
                                     token VARCHAR(255) NOT NULL
+);
+
+-- 5. post 테이블 생성
+CREATE TABLE IF NOT EXISTS post (
+                                    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                    user_id BIGINT NOT NULL,
+                                    study_id BIGINT NOT NULL UNIQUE,
+                                    views BIGINT NOT NULL,
+                                    category VARCHAR(30) NOT NULL,
+                                    title VARCHAR(100),
+                                    summary VARCHAR(100),
+                                    create_at DATETIME DEFAULT  CURRENT_TIMESTAMP,
+                                    delete_at DATETIME,
+
+                                    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+-- 6. postContent 테이블 생성
+CREATE TABLE IF NOT EXISTS post_content (
+                                    post_id BIGINT NOT NULL UNIQUE,
+                                    content VARCHAR(1000) NOT NULL,
+                                    modify_at DATETIME,
+
+                                    CONSTRAINT fk_post_content_post FOREIGN KEY (post_id) REFERENCES post(id)
+);
+
+-- 7. study 테이블 생성
+CREATE TABLE IF NOT EXISTS study (
+                                    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                    post_id BIGINT NOT NULL UNIQUE,
+                                    title VARCHAR(100),
+                                    location varchar(50),
+                                    max_member BIGINT NOT NULL,
+                                    state varchar(10) DEFAULT  'RECRUITING',
+                                    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                                    delete_at DATETIME,
+
+                                    CONSTRAINT fk_study_post FOREIGN KEY (post_id) REFERENCES post(id)
+);
+
+-- 8. study 참여자 테이블 생성
+CREATE TABLE IF NOT EXISTS study_participant (
+                                   id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                   study_id BIGINT NOT NULL,
+                                   user_id BIGINT NOT NULL,
+                                   joined_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                                   status VARCHAR(20) DEFAULT 'APPROVED', -- WAITING, APPROVED, REJECTED, CANCELLED 등
+
+                                   UNIQUE KEY unique_participant (study_id, user_id), -- 중복 신청 방지
+                                   CONSTRAINT fk_participant_study FOREIGN KEY (study_id) REFERENCES study(id),
+                                   CONSTRAINT fk_participant_user FOREIGN KEY (user_id) REFERENCES user(id)
 );
