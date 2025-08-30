f25be7640248f7905491edb957ddd1945ef75b2d
Jonghyeon0429p <bwhdgus0429@naver.com>
2025-08-31 00:35:21 +0900

    feat : LikeMapper 구상
    - LikeMapper를 구현하기 위한 인터페이스 작성

--- src/main/java/com/westudy/like/mapper/LikeMapper.java
+++ src/main/java/com/westudy/like/mapper/LikeMapper.java
@@ -1,5 +1,15 @@
 package com.westudy.like.mapper;
 
-public class LikeMapper {
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
+public interface LikeMapper {
+    void insertLikeComment(long commentId, long userId);
+    void insertLikePost(long postId, long userId);
+    void addLikeCommentCount(long commentId);
+    void addLikePostCount(long postId);
+    void deleteLikeComment(long commentId, long userId);
+    void deleteLikePost(long commentId, long userId);
+    void minusLikeCommentCount(long commentId);
+    void minusLikePostCount(long postId);
 }
