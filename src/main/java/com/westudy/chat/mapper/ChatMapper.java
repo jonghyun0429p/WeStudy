e78825a61118100cd800d87034a5a5bc7cfdfb48
bwhdgus0429 <bwhdgus0429@naver.com>
2026-03-18 21:45:05 +0900

    feat: MyBatis를 이용한 채팅 데이터 저장 및 조회 레이어 구현

---
+++ src/main/java/com/westudy/chat/mapper/ChatMapper.java
@@ -0,0 +1,14 @@
+package com.westudy.chat.mapper;
+
+import com.westudy.chat.dto.ChatMessageResponseDTO;
+import com.westudy.chat.entity.ChatMessage;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface ChatMapper {
+    void insertMessage(ChatMessage chatMessage);
+    List<ChatMessageResponseDTO> findMessagesByStudyId(@Param("studyId") Long studyId);
+}
