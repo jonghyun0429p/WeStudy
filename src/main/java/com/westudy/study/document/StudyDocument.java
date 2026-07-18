0bbb4b0363f403652962825dc4009f3e823fece7
Jonghyeon0429p <bwhdgus0429@naver.com>
2026-07-19 07:23:45 +0900

    feat: StudyDocument 도큐먼트 엔티티 클래스 생성

---
+++ src/main/java/com/westudy/study/document/StudyDocument.java
@@ -0,0 +1,44 @@
+package com.westudy.study.document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.Setting;
+
+@Document(indexName = "studies")
+@Setting(settingPath = "/elasticsearch/settings.json")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudyDocument {
+
+    @Id
+    private String id;
+
+    @Field(type = FieldType.Long)
+    private Long postId;
+
+    @Field(type = FieldType.Long)
+    private Long userId;
+
+    @Field(type = FieldType.Text, analyzer = "nori_synonym_analyzer", searchAnalyzer = "nori_synonym_analyzer")
+    private String title;
+
+    @Field(type = FieldType.Text, analyzer = "nori_synonym_analyzer")
+    private String location;
+
+    @Field(type = FieldType.Integer)
+    private int maxMember;
+
+    @Field(type = FieldType.Keyword)
+    private String state;
+
+    @Field(type = FieldType.Integer)
+    private int approvedMemberCount;
+}
